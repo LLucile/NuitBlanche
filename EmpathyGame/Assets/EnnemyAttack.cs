39591a403df5f947e020ce532751da58dcbc7d90
Lucile <lucile@ymail.com>
2015-10-03 00:57:20 +0200

    Working on Enemy

---
+++ EmpathyGame/Assets/EnnemyAttack.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnnemyAttack : MonoBehaviour {
+
+    public float timeBetweenAttacks = 0.5f;
+    public int attackDamage = 10;
+
+    Animator anim;
+    GameObject player;
+    PlayerHealth playerHealth;
+    EnemyHealth enemyHealth;
+    bool playerInRange;
+    float timer;
+
+	// Use this for initialization
+	void Awake () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<PlayerHealth>();
+        enemyHealth  = GetComponent<EnemyHealth>();
+        anim = GetComponent<Animator>();
+	}
+
+    // Use this for trigger event
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            playerInRange = true;
+        }
+    }
+
+    //Use this for trigger event out
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            playerInRange = false;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if (timer >= timeBetweenAttacks && playerInRange && enemyHealth.currentHealth>0)
+        {
+            Attack();
+        }
+        if (playerHealth.currentHealth <= 0)
+        {
+            //stop moving : player is dead
+        }	
+	}
+
+    void Attack()
+    {
+        timer = 0f;
+        if (playerHealth.currentHealth > 0)
+        {
+            //Send damages to player
+        }
+    }
+}
+
+
---
+++ EmpathyGame/Assets/EnnemyMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnnemyMovement : MonoBehaviour {
+
+    Transform Player;
+    //PlayerHealth playerHealth;
+    //EnemyHealth enemyHealth;
+    NavMeshAgent nav;
+
+	// Use this for initialization
+	void Awake () {
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+        //playerHealth = player.GetComponent <PlayerHealth> ();
+        //enemyHealth = GetComponent <EnemyHealth>();
+        nav = GetComponent<NavMeshAgent>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //if(enemyHealth.currentHealth > 0  && playerHealth.currentHealth > 0)
+        //{
+        nav.SetDestination(player.position);
+        //}
+        //else
+        //{
+        // nav.enabled = false;
+        //}
+	
+	}
+}
