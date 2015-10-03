1fc58df1ce1c983c8e96ef164f0e0b57f5166967
vimock <vi_mock@hotmail.com>
2015-10-03 13:32:41 +0200

    Free rotation for player

--- EmpathyGame/Assets/Script/PlayerController.cs
+++ EmpathyGame/Assets/Script/PlayerController.cs
@@ -3,20 +3,6 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour 
 {
-	public enum PlayerOrientation 
-	{
-		North = 0,
-		NorthEast = 45,
-		East = 90,
-		SouthEast = 135,
-		South = 180,
-		SouthWest = 225,
-		West = 270,
-		NorthWest = 315,	
-	}
-
-	public PlayerOrientation _orientation;
-
 	// Use this for initialization
 	void Start () 
 	{
@@ -34,16 +20,15 @@ public class PlayerController : MonoBehaviour
 		{
 			GoBackward();
 		}		
-	
-		if (Input.GetKeyUp (KeyCode.LeftArrow)) 
+		
+		if (Input.GetAxis ("Horizontal") > 0) 
 		{
 			TurnLeft();
 		}
-
-		if (Input.GetKeyUp (KeyCode.RightArrow)) 
+		else if (Input.GetAxis ("Horizontal") < 0) 
 		{
 			TurnRight();
-		}
+		}	
 	}
 	
 	private void GoForward() 
@@ -58,80 +43,14 @@ public class PlayerController : MonoBehaviour
 
 	private void TurnRight() 
 	{
-		switch (_orientation) 
-		{
-			case PlayerOrientation.North:
-				_orientation = PlayerOrientation.NorthEast;
-				break;
-
-			case PlayerOrientation.NorthEast:
-				_orientation = PlayerOrientation.East;
-				break;
-
-			case PlayerOrientation.East:
-				_orientation = PlayerOrientation.SouthEast;
-				break;
-
-			case PlayerOrientation.SouthEast:
-				_orientation = PlayerOrientation.South;
-				break;
-
-			case PlayerOrientation.South:
-				_orientation = PlayerOrientation.SouthWest;
-				break;
-
-			case PlayerOrientation.SouthWest:
-				_orientation = PlayerOrientation.West;
-				break;
-
-			case PlayerOrientation.West:
-				_orientation = PlayerOrientation.NorthWest;
-				break;
-
-			case PlayerOrientation.NorthWest:
-				_orientation = PlayerOrientation.North;
-				break;
-		}
-		this.transform.eulerAngles = new Vector3(0, (int)_orientation,0);
+		this.transform.Rotate (0, -Time.deltaTime * 360, 0);
+		//this.transform.eulerAngles = new Vector3(0, (int)_orientation,0);
 	}
 
 	private void TurnLeft() 
 	{
-		switch (_orientation) 
-		{
-			case PlayerOrientation.North:
-				_orientation = PlayerOrientation.NorthWest;
-				break;
-				
-			case PlayerOrientation.NorthWest:
-				_orientation = PlayerOrientation.West;
-				break;
-				
-			case PlayerOrientation.West:
-				_orientation = PlayerOrientation.SouthWest;
-				break;
-				
-			case PlayerOrientation.SouthWest:
-				_orientation = PlayerOrientation.South;
-				break;
-				
-			case PlayerOrientation.South:
-				_orientation = PlayerOrientation.SouthEast;
-				break;
-				
-			case PlayerOrientation.SouthEast:
-				_orientation = PlayerOrientation.East;
-				break;
-				
-			case PlayerOrientation.East:
-				_orientation = PlayerOrientation.NorthEast;
-				break;
-				
-			case PlayerOrientation.NorthEast:
-				_orientation = PlayerOrientation.North;
-				break;
-		}
-		this.transform.eulerAngles = new Vector3(0, (int)_orientation,0);
+		this.transform.Rotate (0, Time.deltaTime * 360, 0);
+		//this.transform.eulerAngles = new Vector3(0, (int)_orientation,0);
 	}
 
 }
