379f066c1d58fecf91de054605a2e80b70039e19
Lucile <lucile@ymail.com>
2015-10-03 15:21:03 +0200

    Added game Over Screen

---
+++ EmpathyGame/Assets/GameOverManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverManager : MonoBehaviour {
+
+
+    Animator anim;
+    float restartTimer;
+
+	// Use this for initialization
+	void Start () {
+	    anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player.GetComponent<Health>().currentHealth <= 0)
+        {
+            anim.SetTrigger("GameOver");
+
+            //TODO ADD RESTART CONDITIONS
+        }
+	
+	}
+}
